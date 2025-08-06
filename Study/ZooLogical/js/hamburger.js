b0c3754779e8dd4ab97fa37b0407067e539c2ab3
ozawatokito <ozatoki.ozatoki@gmail.com>
2025-08-06 17:17:05 +0900

    ハンバーガーメニュー作成

---
+++ Study/ZooLogical/js/hamburger.js
@@ -0,0 +1,5 @@
+const hamburger = document.getElementById('hamburger');
+const nav = document.getElementById('nav');
+hamburger.addEventListener('click', () => {
+    nav.classList.toggle('active')
+});
