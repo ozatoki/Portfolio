3cf052bb5b20e4ee6bca2570b143d529290a8725
ozawatokito <ozatoki.ozatoki@gmail.com>
2025-10-27 16:05:53 +0900

    ディレクトリ修正

--- Study/ZooLogical-site/js/hamburger.js
+++
@@ -1,5 +0,0 @@
-const hamburger = document.getElementById("hamburger");
-const nav = document.getElementById("nav");
-hamburger.addEventListener("click", () => {
-  nav.classList.toggle("active");
-});
