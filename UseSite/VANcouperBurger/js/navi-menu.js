780c3ec7884c8ea3bcaa7c43812456141b5cca2e
ozawatokito <ozatoki.ozatoki@gmail.com>
2025-11-23 15:33:04 +0900

    ハンバーガーサイト更新

---
+++ UseSite/VANcouperBurger/js/navi-menu.js
@@ -0,0 +1,9 @@
+const menuBtn = document.getElementById("menuBtn");
+menuBtn.addEventListener("click", () => {
+  menuBtn.classList.toggle("open");
+});
+
+const naviMenu = document.getElementById("naviMenu");
+menuBtn.addEventListener("click", () => {
+  naviMenu.classList.toggle("open");
+});
